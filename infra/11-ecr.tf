5ab75eeaa68f9b5742cea22290cb00c81acc3e8c
Apostolis Tanopoulos <apostolistanopoulos@gmail.com>
2023-08-24 11:58:54 +0300

    [devops] added lifecycle policy for images in ecr repos

--- infra/11-ecr.tf
+++ infra/11-ecr.tf
@@ -17,3 +17,49 @@ resource "aws_ecr_repository" "devops_portfolio_develop_repository" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "devops_portfolio_main_repository_policy" {
+  repository = aws_ecr_repository.devops_portfolio_main_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 4
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "devops_portfolio_develop_repository_policy" {
+  repository = aws_ecr_repository.devops_portfolio_develop_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 4
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
